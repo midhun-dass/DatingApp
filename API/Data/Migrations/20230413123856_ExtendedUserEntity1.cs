25a1e0b8f325d8e541a34106be59101d3593d2a9
midhundas <kmidhundas8@gmail.com>
2023-04-21 09:33:52 +0530

    data seed

---
+++ API/Data/Migrations/20230413123856_ExtendedUserEntity1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendedUserEntity1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublicIf",
+                table: "Photos",
+                newName: "PublicId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublicId",
+                table: "Photos",
+                newName: "PublicIf");
+        }
+    }
+}
