f9a294ddc7fd8e24a7d9e24c64d72213ee0d0376
midhundas <kmidhundas8@gmail.com>
2023-12-30 17:30:15 +0530

    Setting up of message repository

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
 
 namespace API.Interfaces
 {
@@ -12,5 +11,7 @@ namespace API.Interfaces
         Task<Message> GetMessage(int id);
         Task<PagedList<MessageDto>> GetMessagesForUser();
         Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<bool> SaveAllAsync();
+
     }
 }
