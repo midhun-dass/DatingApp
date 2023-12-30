bbf7020ceaa3ddf2f025ddf15b70fd3b91cd01e6
Async <midhundask8@gmail.com>
2023-12-30 16:55:17 +0530

    IMessageRepo

---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+    }
+}
